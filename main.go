749a21d3879bfcd8db623e53443f7628cafe5eff
wberg <wberg@wberg.com>
2024-06-18 16:20:43 +0200

    move gin to own package

--- main.go
+++ main.go
@@ -2,18 +2,12 @@ package main
 
 import (
 	"fmt"
-	"log"
-	"net/http"
 	"os"
 
-	"github.com/gin-gonic/gin"
 	"github.com/wbergg/beerel-roll/dataimport"
 	"github.com/wbergg/beerel-roll/db"
+	"github.com/wbergg/beerel-roll/web"
 )
-
-var ProductNumber struct {
-	Pid int64 `json:"pid"`
-}
 
 func main() {
 	// Setup db
@@ -29,55 +23,6 @@ func main() {
 		os.Exit(0)
 	}
 
-	// Setup Gin
-	r := gin.Default()
-
-	// Serve the HTML page
-	r.GET("/", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "index.html", nil)
-	})
-
-	// Endpoint to get a random beer
-	r.GET("/random-beer", func(c *gin.Context) {
-		beer, err := d.GetRandBeer()
-		if err != nil {
-			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-		} else {
-			c.JSON(http.StatusOK, beer)
-		}
-	})
-
-	// Endpoint to accept a roll
-	r.POST("/accept", func(c *gin.Context) {
-		// Declare a variable of the struct type
-		data := ProductNumber
-
-		// Bind JSON request body to the variable
-		if err := c.ShouldBindJSON(&data); err == nil {
-			// Process the data
-			err := d.ConsumeBeer(data.Pid)
-			if err != nil {
-				log.Fatalf("Could not remove from DB: %v", err)
-			}
-			c.JSON(http.StatusOK, gin.H{"status": "success"})
-		} else {
-			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-		}
-	})
-
-	// Endpoint to get a 5 last beers consumed
-	r.GET("/recent", func(c *gin.Context) {
-		beer, err := d.GetNLastConsumed(5)
-		if err != nil {
-			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-		} else {
-			c.JSON(http.StatusOK, beer)
-		}
-	})
-
-	// Load the HTML template
-	r.LoadHTMLFiles("templates/index.html")
-
 	// Start webserver
-	r.Run(":8080")
+	web.Start(&d)
 }
---
+++ web/web.go
@@ -0,0 +1,68 @@
+package web
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/wbergg/beerel-roll/db"
+)
+
+var ProductNumber struct {
+	Pid int64 `json:"pid"`
+}
+
+func Start(d *db.DBobject) {
+
+	// Setup Gin
+	r := gin.Default()
+
+	// Serve the HTML page
+	r.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.html", nil)
+	})
+
+	// Endpoint to get a random beer
+	r.GET("/random-beer", func(c *gin.Context) {
+		beer, err := d.GetRandBeer()
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		} else {
+			c.JSON(http.StatusOK, beer)
+		}
+	})
+
+	// Endpoint to accept a roll
+	r.POST("/accept", func(c *gin.Context) {
+		// Declare a variable of the struct type
+		data := ProductNumber
+
+		// Bind JSON request body to the variable
+		if err := c.ShouldBindJSON(&data); err == nil {
+			// Process the data
+			err := d.ConsumeBeer(data.Pid)
+			if err != nil {
+				log.Fatalf("Could not remove from DB: %v", err)
+			}
+			c.JSON(http.StatusOK, gin.H{"status": "success"})
+		} else {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		}
+	})
+
+	// Endpoint to get a 5 last beers consumed
+	r.GET("/recent", func(c *gin.Context) {
+		beer, err := d.GetNLastConsumed(5)
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		} else {
+			c.JSON(http.StatusOK, beer)
+		}
+	})
+
+	// Load the HTML template
+	r.LoadHTMLFiles("templates/index.html")
+
+	// Start webserver
+	r.Run(":8080")
+}
